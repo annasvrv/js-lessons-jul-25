abcda22d76f614c6cffd676e18a292a6e7bc9701
“Anna <suvorova.a@brainrocket.com>
2025-08-10 12:18:39 +0300

    day 1 with gpt5

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Day 1
+
+// Exercise 1
+console.log("Anna");
+
+// Exercise 2
+let myName = "Sara";
+let myAge = 32;
+let myHobby = "reading";
+
+console.log(`Hi, I'm ${myName}, I'm ${myAge} years old and I love ${myHobby}.`);
