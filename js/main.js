c1857b9119093e46eb803513d4c4dd37c92fdad2
Anna <anna.svrva@gmail.com>
2025-08-10 12:50:55 +0300

    correction in Day 1

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@
 console.log("Anna");
 
 // Exercise 2
-let myName = "Sara";
-let myAge = 32;
-let myHobby = "reading";
+const myName = "Sara";
+const myAge = 32;
+const myHobby = "reading";
 
 console.log(`Hi, I'm ${myName}, I'm ${myAge} years old and I love ${myHobby}.`);
