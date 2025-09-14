32803c29af59f953c1313315f83acf4b3c5a1139
Anna <anna.svrva@gmail.com>
2025-09-14 18:56:59 +0300

    day 6 exercise

--- js/day-6.js
+++ js/day-6.js
@@ -5,11 +5,26 @@
 //  Selecting Elements: By ID, By Class, By Tag Name
 //  Modern Way — querySelector / querySelectorAll
 
-const firstParagraph = document.querySelector(".text"); // first match
-const allParagraphs = document.querySelectorAll(".text"); // NodeList of all matches
+// const firstParagraph = document.querySelector(".text"); // first match
+// const allParagraphs = document.querySelectorAll(".text"); // NodeList of all matches
 
 //  Accessing / Reading Content
 
-console.log(title.innerText);    // visible text
-console.log(title.textContent);  // all text (including hidden)
-console.log(title.innerHTML);    // HTML inside
+// console.log(title.innerText);    // visible text
+// console.log(title.textContent);  // all text (including hidden)
+// console.log(title.innerHTML);    // HTML inside
+
+// ******************************************************************
+
+const title = document.getElementById("main-title");
+console.log(title.textContent);
+
+const paragraph = document.getElementsByClassName("description")[0];
+console.log(paragraph.textContent);
+
+let listItems = document.querySelectorAll("li");
+
+for (let i = 0; i <= listItems.length; i++) {
+  item = listItems[i].textContent;
+  console.log(item);
+}
