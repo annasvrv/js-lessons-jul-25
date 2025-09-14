e58322b5547aa7c303defb4b852610108bf2a42d
Anna <anna.svrva@gmail.com>
2025-09-14 13:36:56 +0300

    day 4 exercise

---
+++ js/day-4.js
@@ -0,0 +1,124 @@
+//  Day 4: Control Flow
+
+// if/else  Basic conditional logic
+
+// switch Cleaner than multiple if statements when checking the same variable:
+
+let day = "Monday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Friday":
+    console.log("Weekend is near");
+    break;
+  default:
+    console.log("Midweek day");
+}
+
+//  Loops
+
+// - for
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+// while
+
+let count = 1;
+while (count <= 5) {
+  console.log(count);
+  count++;
+}
+
+// do...while   Executes at least once:
+
+let num = 5;
+do {
+  console.log(num);
+  num++;
+} while (num <= 7);
+
+// break & continue
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) break; // stops the loop
+  console.log(i);
+}
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) continue; // skips this iteration
+  console.log(i);
+}
+
+// ******************************************************
+
+// Exercise
+
+// Write an if/else that checks if a number is positive, negative, or zero.
+
+let num1 = 0;
+
+if (num1 > 0) {
+  console.log("positive number");
+} else if (num1 < 0) {
+  console.log("negative number");
+} else {
+  console.log("it is a zero");
+}
+
+// Use a switch to print the name of the day for numbers 1–7.
+
+let weekDay = 5;
+
+switch (weekDay) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thurday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Weekday");
+}
+
+// Write a for loop that prints all even numbers from 1 to 20.
+
+for (let i = 1; i <= 20; i++) {
+  if (i%2 === 0) {
+    console.log(i);
+  } else {
+    continue;
+  }
+}
+
+// Write a while loop that counts down from 10 to 1.
+
+let countNumber = 10;
+while (countNumber > 0) {
+  console.log(countNumber);
+  countNumber = countNumber - 1;
+}
+
+
+// Use a for loop with continue to skip the number 13 while printing numbers from 10 to 20.
+for (let n = 10; n <=20; n++) {
+  if (n === 13) continue;
+  console.log(n);
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Day 1 Variables: var, let, const
+// Day 1 Variables: var, let, const ******************************************
 
 // Exercise 1
 console.log("Anna");
@@ -10,7 +10,7 @@ const myHobby = "reading";
 
 console.log(`Hi, I'm ${myName}, I'm ${myAge} years old and I love ${myHobby}.`);
 
-// Day 2 Data Types
+// Day 2 Data Types **********************************************************
 
 // Exercise 1 - Create variables for each data type (primitive + reference).
 
@@ -69,7 +69,7 @@ const myFunction = function () {
 testFunction();
 myFunction();
 
-// Day 3 Operators & Type Coercion
+// Day 3 Operators & Type Coercion ******************************************
 
 // Exercise 1 - Create two numbers and perform all arithmetic operations on them.
 
