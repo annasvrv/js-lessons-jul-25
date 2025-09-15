cd69f5fbbc92d0400e135413dbc91a3ea115d0e5
“Anna <suvorova.a@brainrocket.com>
2025-09-15 11:26:50 +0300

    day 7 start

---
+++ js/day-7.js
@@ -0,0 +1,10 @@
+// In your day-7.js:
+
+// Change the <h1> text to "Welcome to DOM Manipulation".
+// Change the <p> text color to green.
+// Change the <a> link to point to https://google.com.
+// Add a highlight class to the <div>.
+// Update the <img> src and alt attributes.
+
+const title = document.getElementById("main-title");
+title.textContent = "Welcome to DOM Manipulation";
