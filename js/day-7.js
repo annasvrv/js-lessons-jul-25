cca086397d628df29ab42ca8028a6ae52dcc9be8
“Anna <suvorova.a@brainrocket.com>
2025-09-15 14:16:23 +0300

    day 7 exercise

--- js/day-7.js
+++ js/day-7.js
@@ -20,5 +20,5 @@ elem.classList.add("highlight");
 // Update the <img> src and alt attributes.
 const image = document.querySelector("img");
 image.src =
-  "https://www.google.com/imgres?q=cat&imgurl=https%3A%2F%2Fi.guim.co.uk%2Fimg%2Fmedia%2F327aa3f0c3b8e40ab03b4ae80319064e401c6fbc%2F377_133_3542_2834%2Fmaster%2F3542.jpg%3Fwidth%3D465%26dpr%3D1%26s%3Dnone%26crop%3Dnone&imgrefurl=https%3A%2F%2Fwww.theguardian.com%2Fus-news%2F2025%2Fmay%2F22%2Fginger-cats-mystery-solved&docid=gg7zTLzu9y87gM&tbnid=0vHbjWlk-92axM&vet=12ahUKEwid2_2JtNqPAxXDT6QEHcSbOZsQM3oECBoQAA..i&w=465&h=372&hcb=2&ved=2ahUKEwid2_2JtNqPAxXDT6QEHcSbOZsQM3oECBoQAA";
+  "https://www.freepik.com/free-psd/beautiful-cat-portrait-isolated_38310664.htm#fromView=keyword&page=1&position=0&uuid=2173c628-b771-456d-b698-0173b662f0b8&query=Cat+png";
 image.alt = "ginger cat";
